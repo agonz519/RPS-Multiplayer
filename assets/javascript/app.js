0c40ce9eca15ea87deb152277b59343f643cf81b
Alexander <agonz519@gmail.com>
2018-03-30 22:23:16 -0400

    Yo my code is straight JACKED UP. Pushing what I have to try a different approach. In case its worse, Ill roll back to this. Here we go weeeeeeeeeeeeeeeeeeeee

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -24,38 +24,41 @@ $(document).ready(function(){
 	var player2Wins = 0;
 	var player2Losses = 0;
 	var player2Ties = 0;
-	var turn = 0;
+	var turn = 1;
 
-	// SUBMIT NAME CLICK EVENT
-
-
+	$("#p1Choice").hide();
+	$("#p2Choice").hide();
 
 	db.ref().on("value", function(snapshot) {
-		console.log(snapshot.val());
+		console.log("Snapshot value currently equals " + snapshot.val());
 
 		if (snapshot.child("players/1").exists()) {
-			alert("EUREKA!!!!");
+			console.log("Player 1 exists.")
 			var player1 = snapshot.child("players/1").val();
 			$('#player1name').text(player1.name);
+			$('#p1Stats').text("Wins: " + player1Wins + " | Losses: " + player1Losses + " | Ties: " + player1Ties);
 		} else {
-			alert("doh!!");
+			console.log("Player 1 does not exist.");
+			$('#player1name').text("");
 		}
-
 		if (snapshot.child("players/2").exists()) {
-			alert("Player 2 exists");
+			console.log("Player 2 exists.");
 			var player2 = snapshot.child("players/2").val();
 			$('#player2name').text(player2.name);
+			$('#p2Stats').text("Wins: " + player2Wins + " | Losses: " + player2Losses + " | Ties: " + player2Ties);
 		} else {
-			alert("player 2 does not exist");
+			console.log("Player 2 does not exist.");
+			$('#player2name').text("");
 		}
 
+		// SUBMIT NAME CLICK EVENT
 		$("#submitName").on("click", function(event) {
 			// PREVENT RELOAD OF PAGE
 			event.preventDefault();
 
-			var name = $('#nameInput').val().trim();
+
 			if (snapshot.child("players/1").exists() === false) {
-				player1Name = name;
+				player1Name = $('#nameInput').val().trim();
 				db.ref("players/1").update({
 					name: player1Name,
 					wins: player1Wins,
@@ -63,9 +66,10 @@ $(document).ready(function(){
 					ties: player1Ties,
 					choice: player1Choice
 				});
+				$('.form-group').text("Hello " + player1Name + "! You are player 1.");
 				db.ref("players/1").onDisconnect().remove();
 			} else if (snapshot.child("players/2").exists() === false){
-				player2Name = name;
+				player2Name = $('#nameInput').val().trim();
 				db.ref("players/2").update({
 					name: player2Name,
 					wins: player2Wins,
@@ -73,12 +77,72 @@ $(document).ready(function(){
 					ties: player2Ties,
 					choice: player2Choice
 				});
+				$('.form-group').text("Hello " + player2Name + "! You are player 2.");
 				db.ref("players/2").onDisconnect().remove();
 			} else {
 				alert("Sorry, game is full. Please wait...");
 				$('#nameInput').val("");
 			}
 		});
+
+		if (snapshot.child("players/1").exists() && snapshot.child("players/2").exists()) {
+			db.ref().update({
+				turn: turn
+			});
+			$("#p1Choice").show();
+			if (turn = 1) {
+				$("#result").text("Waiting on player 1");
+				$("#rock1").on("click", function() {
+					$("#rock1").css("font-weight", "bold");
+					player1Choice = "rock";
+					console.log("Player 1 chose " + player1Choice);
+					db.ref("players/1").update({
+						choice: player1Choice,
+					});
+					db.ref().update({
+						turn: turn++
+					});
+				});
+			}
+			if (turn = 2) {
+				$("#result").text("Waiting on player 2");
+				$("#paper2").on("click", function() {
+					$("#paper2").css("font-weight", "bold");
+					player2Choice = "paper";
+					console.log("Player 2 chose " + player2Choice);
+					db.ref("players/2").update({
+						choice: player2Choice,
+					});
+					db.ref().update({
+						turn: turn++
+					});
+				});
+			}
+			if (turn = 3) {
+				if (player1Choice === player2Choice) {
+					db.ref("players/1").update({ties: player1Choice++});
+					db.ref("players/2").update({ties: player2Choice++});
+				} else if (player1Choice === 'rock' && player2Choice === "scissors") {
+					db.ref("players/1").update({wins: player1Wins++});
+					db.ref("players/2").update({wins: player2Losses++});
+					$("#result").text("PLAYER 1 ROCKED YOU");
+				} else if (player1Choice === 'paper' && player2Choice === "rock") {
+					db.ref("players/1").update({wins: player1Wins++});
+					db.ref("players/2").update({wins: player2Losses++});
+				} else if (player1Choice === 'scissors' && player2Choice === "paper") {
+					db.ref("players/1").update({wins: player1Wins++});
+					db.ref("players/2").update({wins: player2Losses++});
+				} else {
+					db.ref("players/1").update({wins: player1Losses++});
+					db.ref("players/2").update({wins: player2Wins++});
+				}
+				turn = 1;
+				db.ref().update({
+					turn: turn
+				});
+			}
+		}
+
 
 	}, function(errorObject) {
 		console.log("The read failed: " + errorObject.code);
